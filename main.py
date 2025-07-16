fe1c1ffe6bd96fbaec7e91e03453e2cc989bb901
hmnbstn <bastien.hamon17@gmail.com>
2025-07-16 15:32:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from engine.simulator import load_fighter, simulate_fight
 
+f1 = load_fighter("fighters/jon_jones.json")
+f2 = load_fighter("fighters/khabib.json")
+
+result = simulate_fights(f1, f2)
+
+print(f"\n {result['winner']} wins the fight!")
+print(f"Scores - {f1['name']}: {rsult['score1']} | {f2['name']}: {result['score2']}\n")
